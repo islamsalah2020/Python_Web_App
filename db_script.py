77e6797b0366f76a2ef912f4c653bbf9393ec85b
islamsalah2020 <islamsalah830@gmail.com>
2022-07-13 14:57:22 +0200

    update code

---
+++ db_script.py
@@ -0,0 +1,16 @@
+python manage.py makemigrations projects
+python manage.py migrate projects
+
+python manage.py shell
+from projects.models import Project
+
+p1 = Project(title='My First Project',description='A web development project.',technology='Django',image='img/project1.png')
+p1.save()
+
+
+p2 = Project(title='My Second Project',description='Another web development project.',technology='Flask',image='img/project2.png')
+p2.save()
+
+
+p3 = Project(title='My Third Project',description='A final development project.',technology='Django',image='img/project3.png')
+p3.save()
